bf0bf4cbc299c0fee23ec9641b42bbdd7f44d46b
McCune1224 <alexmccune1224@gmail.com>
2024-09-15 21:36:55 -0400

    multi-line completion toggle

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,7 +2,7 @@ return {
   'supermaven-inc/supermaven-nvim',
   config = function()
     require('supermaven-nvim').setup {
-      disable_inline_completion = true,
+      -- disable_inline_completion = true,
       disable_keymaps = true,
     }
     -- Disable supermaven by default
