4ff8aa8676040b38351728fc4641f1bf83c51805
Alex McCune <alexmccune1224@gmail.com>
2025-04-24 10:46:03 -0400

    mini notify

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,8 +10,8 @@ return {
       'saghen/blink.cmp',
 
       -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      -- { 'j-hui/fidget.nvim', opts = {} },
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
