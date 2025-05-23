51b6cdb730e04d8019c4df8324beb8759de90573
Alex McCune <alexmccune1224@gmail.com>
2025-05-23 08:30:26 -0400

    disable smear on win device

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,5 +1,8 @@
 return {
-  'sphamba/smear-cursor.nvim',
-  opts = { -- Default  Range
+  {
+    'sphamba/smear-cursor.nvim',
+    enabled = vim.fn.has 'win32' == 0, -- disables on Windows
+    opts = { -- Default  Range
+    },
   },
 }
