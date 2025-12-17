2b4f2447b81e7b62eef146dad24bdb725f5db6e1
McCune1224 <alexmccune1224@gmail.com>
2025-12-17 13:56:22 -0500

    remove mcp

--- lua/plugins/mcp.lua
+++ lua/plugins/mcp.lua
@@ -1,23 +1,24 @@
-return {
-  'ravitemer/mcphub.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
-  opts = {
-    extensions = {
-      avante = {
-        make_slash_commands = true,
-      },
-      copilotchat = {
-        enabled = true,
-        convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
-        convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
-        add_mcp_prefix = false, -- Add "mcp_" prefix to function names
-      },
-    },
-  },
-  config = function()
-    require('mcphub').setup()
-  end,
-}
+return {}
+-- return {
+--   'ravitemer/mcphub.nvim',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--   },
+--   build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
+--   opts = {
+--     extensions = {
+--       avante = {
+--         make_slash_commands = true,
+--       },
+--       copilotchat = {
+--         enabled = true,
+--         convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
+--         convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+--         add_mcp_prefix = false, -- Add "mcp_" prefix to function names
+--       },
+--     },
+--   },
+--   config = function()
+--     require('mcphub').setup()
+--   end,
+-- }
